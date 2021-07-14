3115a81a83a3b73d9442d1290155540db5c031ed
github-action-benchmark <github@users.noreply.github.com>
2021-07-14 07:25:00 +0000

    add pipeline (cargo) benchmark result for 25d18c0b74ccf9e51f986daa3b2b98c0109f827a

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1626247500015,
+  "repoUrl": "https://github.com/red75prime/hyper",
+  "entries": {
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "25d18c0b74ccf9e51f986daa3b2b98c0109f827a",
+          "message": "feat(ffi): add hyper_request_on_informational\n\nThis defines an extension type used in requests for the client that is\nused to setup a callback for receipt of informational (1xx) responses.\nThe type isn't currently public, and is only usable in the C API.",
+          "timestamp": "2021-07-12T15:43:58-07:00",
+          "tree_id": "da554dd5b853aebeb130f781735d452941b06e1e",
+          "url": "https://github.com/red75prime/hyper/commit/25d18c0b74ccf9e51f986daa3b2b98c0109f827a"
+        },
+        "date": 1626247493191,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 58956,
+            "range": "± 19288",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
